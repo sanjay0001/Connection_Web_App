921bafec9e7be23f993922b0276299828f643138
shrish <59884160+MSHRISH@users.noreply.github.com>
2022-04-01 11:51:38 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-
-class register(FlaskForm):
-	roll_no=StringField(label='Rollno')
-	password1=StringField(label='Password')
-	login=SubmitField(label='Login')
-	
